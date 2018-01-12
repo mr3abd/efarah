81e1e7473382cecc004f58a6879bf8d125865d9a
abdulrhman haider <abd097@hotmail.com>
2018-01-13 01:04:55 +0200

    fix ajax

--- app/controllers/advices_controller.rb
+++ app/controllers/advices_controller.rb
@@ -4,7 +4,7 @@ class AdvicesController < ApplicationController
   # GET /advices
   # GET /advices.json
   def index
-    @advices = Advice.all
+    @advices = Advice.order("created_at DESC")
     @advice = Advice.new
   end
 
@@ -25,21 +25,17 @@ class AdvicesController < ApplicationController
   # POST /advices
   # POST /advices.json
   def create
+    @advices = Advice.order("created_at DESC")
 
-    @advice = Advice.new(advice_params)
+    @advice = Advice.create(advice_params)
 
     respond_to do |format|
       if @advice.save
-@advices = @advice
-        format.html { redirect_to @advice, notice: 'Advice was successfully created.' }
 
-        format.json { render :show, status: :created, location: @advice }
+        format.json { head :no_content }
         format.js
       else
-        format.html { render :new }
         format.json { render json: @advice.errors, status: :unprocessable_entity }
-        format.js
-
       end
     end
   end
